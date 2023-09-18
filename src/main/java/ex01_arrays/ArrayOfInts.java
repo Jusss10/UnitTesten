9c2717008fb2d10a1c954c73cfb3036fa83fecb5
Justine Dor <118171057+Jusss10@users.noreply.github.com>
2023-09-18 15:56:39 +0200

    bekijk op github voor andere mogelijke uitkomsten

--- src/main/java/ex01_arrays/ArrayOfInts.java
+++ src/main/java/ex01_arrays/ArrayOfInts.java
@@ -32,7 +32,7 @@ public class ArrayOfInts {
 
     public int sumOfXLargest(int[] arrayOfInts, int x) {
         if(arrayOfInts == null || arrayOfInts.length == 0) return 0;
-        if(x== 0) return 0;
+        if(x <= 0) return 0;
 
         int[] copy = Arrays.copyOf(arrayOfInts, arrayOfInts.length);
         Arrays.sort(copy);
@@ -43,10 +43,38 @@ public class ArrayOfInts {
             return sumOfInts(Arrays.copyOfRange(copy, copy.length -x, copy.length));
 
     }
+    public int countMostPopularNumber(int[] arrayOfInts) {
+        if (arrayOfInts == null || arrayOfInts.length == 0) return 0;
+        {
+
+            int[] copy = Arrays.copyOf(arrayOfInts, arrayOfInts.length);
+            Arrays.sort(copy);
+
+            int maxcount = 0;
+            int element_having_max_freq = 0;
+            for (int i = 0; i < copy.length; i++) {
+                int count = 0;
+                for (int j = 0; j < copy.length; j++) {
+                    if (arrayOfInts[i] == arrayOfInts[j]) {
+                        count++;
+                    }
+                }
+                if (count > maxcount) {
+                    maxcount = count;
+                    element_having_max_freq = arrayOfInts[i];
+                }
+            }
+            //kijk online op github voor de rest
+            return maxcount;
+        }
+
+    }
 
     public static void main(String[] args) {
         ArrayOfInts ex1 = new ArrayOfInts();
         int result = ex1.sumOfInts(new int[]{1, 2, 3, 4});
         System.out.println(result);
     }
+
+
 }
